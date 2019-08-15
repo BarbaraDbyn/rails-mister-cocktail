a50a7b22d8aa88f91110bf7fe75ba92ffd013d45
Barbara Debreyne <barbara.debreyne@orange.fr>
2019-08-15 15:26:04 +0200

    Back part

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning all...'
+Ingredient.destroy_all
+
+puts 'Creating ingredient...'
+Ingredient.create!(name: 'lemon')
+Ingredient.create!(name: 'ice')
+Ingredient.create!(name: 'mint leaves')
+
+puts 'Creating cocktails...'
+Cocktail.create!(name: 'Mint Julep')
+Cocktail.create!(name: 'Whiskey Sour')
+Cocktail.create!(name: 'Mojito')
+
+puts 'Creating doses...'
+Dose.create!(cocktail_id: 'Mint Julep', ingredient_id: 'lemon', description: 'super cocktail')
